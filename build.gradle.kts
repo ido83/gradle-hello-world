3392bc61d826c880dd8bc6b18e63d522a5ab4f83
Max Skalt <maxims@spiralsolutions.com>
2024-04-18 12:58:54 +0300

    b

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-version = "1.0.0"  // b. Set Jar version to 1.0.0
+version = "1.0.0"  // b. Set Jar version  to 1.0.0
 group = "com.ido"
 description = "HelloWorld"
 
