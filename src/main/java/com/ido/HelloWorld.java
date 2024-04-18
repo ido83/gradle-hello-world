04df68b579586d9991c1128e00ffe410413b8510
Max Skalt <maxims@spiralsolutions.com>
2024-04-18 14:11:41 +0300

    some change

--- src/main/java/com/ido/HelloWorld.java
+++ src/main/java/com/ido/HelloWorld.java
@@ -6,6 +6,6 @@ package com.ido;
 public class HelloWorld {
     public static void main(String[] args) {
         // Replace "Keplerian Explorer" with your actual name or desired identifier
-        System.out.println("Hello World from  Max Skalt!");
+        System.out.println("Hello World from  Max Skalt!!!!!");
     }
 }
